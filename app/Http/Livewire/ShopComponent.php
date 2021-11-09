20b3a338eeaf7c4478377a3150f986f50d5a6690
AP-Porag <smauhp@gmail.com>
2021-11-09 15:28:57 +0600

    cart complete

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -3,11 +3,19 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Cart;
 
 class ShopComponent extends Component
 {
+    public function store($product_id, $product_name, $product_price)
+    {
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
+        Session::flash('success_message', 'Item added in cart');
+        return redirect()->route('cart');
+    }
     use WithPagination;
     public function render()
     {
